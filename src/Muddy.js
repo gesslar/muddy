d1d185f896628df7ae831d8eceb9bd8ab074f26b
gesslar <1266935+gesslar@users.noreply.github.com>
2026-01-10 22:57:05 -0500

    01-10-boomshakalaka: 2026-01-10 22:57 - quick save

--- src/Muddy.js
+++ src/Muddy.js
@@ -554,6 +554,13 @@ export default class Muddy {
     const size = await mpackageFile.size()
 
     glog.info(`'${mpackageFile.path}' written to disk (${size.toLocaleString()} bytes)`)
+
+    // A robot made me return context even though there's nothing to do now.
+    // I hope the ether enjoys the context! Yumyum!
+    //
+    // Addendum, yes, there IS one more step? It's #cleanUp, but it doesn't use
+    // the context. Pedantry doesn't look good on you!
+    return ctx
   }
 
   #cleanUp = async() => {
